37c6eeb7d57de61044d8a261d7c591dc158fad35
Sirikon <sirikon2@gmail.com>
2012-12-11 17:59:21 +0100

    Now diffs Chests and Double Chests

--- src/sirilog/listenchest.java
+++ src/sirilog/listenchest.java
@@ -10,8 +10,10 @@ import org.bukkit.event.inventory.InventoryOpenEvent;
 public final class listenchest implements Listener {
     @EventHandler
     public void onInventoryOpenEvent(InventoryOpenEvent event) throws IOException{
-        if (event.getInventory().getHolder() instanceof Chest || event.getInventory().getHolder() instanceof DoubleChest){
-            logsave.savebl(event.getPlayer().getName(), "opened", event.getInventory().getType().toString(), event.getPlayer().getLocation().getBlockX(), event.getPlayer().getLocation().getBlockY(), event.getPlayer().getLocation().getBlockZ(), event.getPlayer().getWorld().getName());
+        if (event.getInventory().getHolder() instanceof Chest){
+            logsave.savebl(event.getPlayer().getName(), "opened", "CHEST", event.getPlayer().getLocation().getBlockX(), event.getPlayer().getLocation().getBlockY(), event.getPlayer().getLocation().getBlockZ(), event.getPlayer().getWorld().getName());
+        }else if (event.getInventory().getHolder() instanceof DoubleChest){
+            logsave.savebl(event.getPlayer().getName(), "opened", "DOUBLE_CHEST", event.getPlayer().getLocation().getBlockX(), event.getPlayer().getLocation().getBlockY(), event.getPlayer().getLocation().getBlockZ(), event.getPlayer().getWorld().getName());
         }
     }
 }
